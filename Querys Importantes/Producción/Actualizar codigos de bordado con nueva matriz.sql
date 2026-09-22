2a7a251d3347085896a8fbb284f969842e29bddf
LCA-rodrigoramirez <rodrigo.ramirez@league91.com>
2026-09-22 15:42:05 -0600

    Update Actualizar codigos de bordado con nueva matriz.sql
    Agregando nueva Location para SP de Bordado

--- Querys Importantes/Producción/Actualizar codigos de bordado con nueva matriz.sql
+++ Querys Importantes/Producción/Actualizar codigos de bordado con nueva matriz.sql
@@ -163,7 +163,7 @@ INNER JOIN
 							CASE 
 								WHEN [Location] LIKE 'Left Chest%' OR [Location] LIKE 'Right Chest%' 
 							OR [Location] LIKE '%Sleeve%' OR [Location] LIKE '%Leg%' OR [Location] LIKE '%HIP%' OR [Location] LIKE '%POCKET%' OR [Location] LIKE '%SHOULDER%'
-							OR [Location] LIKE '%ADULT CENTER CHEST%' OR [Location] LIKE '%COLLAR%' OR [Location] LIKE '%Cuff%' OR [Location] LIKE '%Back Neck%' THEN CONCAT(CAST(Num_Applique as VARCHAR),'S',CodeStitches)
+							OR [Location] LIKE '%ADULT CENTER CHEST%' OR [Location] LIKE '%Medium Center Chest%' OR [Location] LIKE '%COLLAR%' OR [Location] LIKE '%Cuff%' OR [Location] LIKE '%Back Neck%' THEN CONCAT(CAST(Num_Applique as VARCHAR),'S',CodeStitches)
 						WHEN [Location] like '%Front%' or [Location] like '%Back%' or [Location] like '%Rear%' THEN CONCAT(CAST(Num_Applique as VARCHAR),'L',CodeStitches)
 							END
 						WHEN AppliqueMaterial = 'Jersey' AND Num_Applique > 0 THEN CONCAT(CAST(Num_Applique as VARCHAR),CodeStitches)							
@@ -441,11 +441,11 @@ SELECT
 	,CE.LogoStyle
 	,CE.[Location]
 -- UPDATE OD SET
--- 	Comments26 = 'MIS2'
+-- 	Comments26 = CE.Codes
 FROM #TB_OrdersExport AS OE
 LEFT  JOIN #TB_CodesEMB AS CE ON OE.OrderID = CE.OrderID
 INNER JOIN [192.168.1.53].LCA.dbo.Orders AS OD WITH(NOLOCK) ON OE.OrderID = OD.OrderID
-WHERE ItemDetailID in (5733214)
+WHERE ItemDetailID in (6242420)
 return
 
 --SELECT * FROM #TB_CodesEMB WHERE PONumber = 'ORD-5557634'
